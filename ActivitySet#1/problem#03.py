5a9cd699823b518130e8c2b04281f64186ee5093
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-12 14:56:23 +0000

    functions

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,8 +1,11 @@
 
 # Variables, Expressions & Statements
-
-hrs = float(input("enter Hours:"))
-rate=float(input("enter Rate :"))
-Earning =hrs*rate
-print("Pay:",Earning)
+def Earning():
+    hrs = float(input("enter Hours:"))
+    rate=float(input("enter Rate :"))
+    Earning =hrs*rate
+    print("Pay:",Earning)
+def main():
+    Earning()
+main()
 
