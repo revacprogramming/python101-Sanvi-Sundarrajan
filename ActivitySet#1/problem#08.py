0cdb7b50a4460463afc57c88fc1a2504e295be6f
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-13 17:05:51 +0000

    coded file handling

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,15 +1,21 @@
 # Files
 # Use the file name mbox-short.txt as the file name
-
-fname = input("Enter file name: ")
-fhand = open(fname)
-count = 0
-average = 0
-for line in fhand:
-    line=line.rstrip()
-    if not line.startswith("X-DSPAM-Confidence:") : 
-        continue
-    average += float(line[20:])
-    count = count + 1
-    
-print("Average spam confidence:", (average/count))
+def filehandle():
+	fname = input("Enter file name: ")
+	fhand = open(fname)
+	count = 0
+	average = 0
+	for line in fhand:
+	    line=line.rstrip()
+	    if not line.startswith("X-DSPAM-Confidence:") : 
+	        continue
+	    average += float(line[20:])
+	    count = count + 1
+	final=average/count
+	return final
+def output(final):		
+	print("Average spam confidence:", final)
+def main():
+	final=filehandle()
+	output(final)
+main()	
