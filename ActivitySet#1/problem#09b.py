0b5d1d36e528e4dad0987c464e8ee0ca30f221e4
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-15 14:36:01 +0000

    #09b

--- ActivitySet#1/problem#09b.py
+++ ActivitySet#1/problem#09b.py
@@ -1,2 +1,12 @@
 #lists
 fhand = open("mbox-short.txt")
+#fhand = open(fname)
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    	words = line.split()                     
+        print words[1]                        
+        count = count + 1
+
+print "There were", count, "lines in the file with From as the first word"
