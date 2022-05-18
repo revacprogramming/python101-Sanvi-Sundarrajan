769567eeb897d370d5bf5665fcfde2cecac95111
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-18 17:19:16 +0000

    working fine

--- ActivitySet#1/problem#09b.py
+++ ActivitySet#1/problem#09b.py
@@ -2,9 +2,9 @@
 fhand = open("dataset/mbox-short.txt")
 count=0
 for line in fhand:
-    line = line.rstrip()#works even without this , this is cheap
-    if line.startswith('From '):
-     	words = line.split()                     
-        print (words[1])                        
-        count = count + 1
+  if line.startswith('From '):
+    words = line.split()                     
+    print(words[1])                       
+    count = count + 1
+
 print ("There were", count, "lines in the file with From as the first word")
