cf85587e93608a3ceb14a80b5b3ce2de9a4d3fdd
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-18 16:43:47 +0000

    9 in functions

--- ActivitySet#1/problem#09b.py
+++ ActivitySet#1/problem#09b.py
@@ -1,5 +1,5 @@
 #Lists
-
+def input:
 fhand = open("dataset/mbox-short.txt")
 count=0
 for line in fhand:
@@ -8,5 +8,4 @@ for line in fhand:
      	words = line.split()                     
         print (words[1])                        
         count = count + 1
-
 print ("There were", count, "lines in the file with From as the first word")
