2e8958066c8b141412e4e4e98454ff00134c25d8
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-19 16:12:43 +0000

    set 2 #2

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,17 +1,15 @@
 
 def add(a, b):
-    pass  # ...
-
-
-def output(a, b, sum):
-    pass  # ...
+    c = float(a)+float(b) 
+    return c
+def output(a, b, sum_):
+  print("The sum of ",a ,"and", b ,"is",sum_)  
 
 
 def main():
     a, b = input_two_numbers()
-    sum = add(a, b)
-
-    output(a, b, sum)
+    sum_ = add(a, b)
+		output(a, b, sum)
 
 if __name__ == '__main__':
     main()
