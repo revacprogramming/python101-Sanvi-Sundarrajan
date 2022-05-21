d8c479b147a8467f5cb151280a01bbe71d53f84e
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-21 11:37:03 +0000

    set 2 (2)

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,16 +1,16 @@
 def input_two_numbers():
-  a = input("Enter the first number")
-  b = input("Enter the second number")
-	return a,b
+    a = input("Enter the first number")
+    b = input("Enter the second number")
+    return a,b
 def add(a, b):
-  c = float(a)+float(b) 
-  return c
+    c = float(a)+float(b) 
+    return c
 def output(a, b, sum_):
-  print("The sum of ",a ,"and", b ,"is",sum_)  
+    print("The sum of ",a ,"and", b ,"is",sum_)  
 def main():
-  a, b = input_two_numbers()
-  res = add(a, b)
-  output(a, b, res)
+    a, b = input_two_numbers()
+    res = add(a, b)
+    output(a, b, res)
 
 if __name__ == '__main__':
     main()
