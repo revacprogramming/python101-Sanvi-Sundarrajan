a81eb87d6b5ffefaa9d1051881b270e6476a808b
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-25 15:59:30 +0000

    11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,2 +1,12 @@
 # Tuples
 filename = "dataset/mbox-short.txt"
+filehandle=open(filename)
+counts{}
+
+
+for line in filehandle:
+	if line.startswith("From "):
+		time=line.split()[5].split[":"]
+		counts [time] = counts.get(time, 0) + 1
+
+
