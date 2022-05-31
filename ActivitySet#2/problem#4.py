ea161a10207cf6e9f580974f8732e6e8e2f5a49c
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-05-31 17:05:18 +0000

    #2#4

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -6,22 +6,22 @@ def get_cs():
 	return cs
 
 def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
+  lot=cs.split()
+		return lot
 
 def lot_to_cs(lot):
-    """convert list of strings to connected string"""
+  return ''.join(lot)
 
 
 def main():
-    cs=get_cs()
+  cs=get_cs()
 
-    lot=cs_to_lot(cs)  # convert connect string to list of tuples
-    print(lot)
+  lot=cs_to_lot(cs)  # convert connect string to list of tuples
+  print(lot)
 
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
+  cs=lot_to_cs(lot)  # convert list of strings to connect string
+  print(cs)
 
 
 if __name__ == '__main__':
-    main()
+  main()
