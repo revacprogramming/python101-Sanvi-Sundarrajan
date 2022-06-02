61e391b5138e7a94835aeb84170d4688ba92ae59
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-06-02 10:25:42 +0000

    2,#4

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,26 +1,25 @@
 
 
 def get_cs():
-    
-	cs=input("Enter a string")
-	return cs
+    cs=input("Enter a string")
+    return cs
 
 def cs_to_lot(cs):
-  lot=cs.split()
-	return lot
+    lot=cs.split()
+    return lot
 
 def lot_to_cs(lot):
-  return ''.join(lot)
+    return ''.join(lot)
 
 
 def main():
-  cs=get_cs()
+    cs=get_cs()
 
-  lot=cs_to_lot(cs)  # convert connect string to list of tuples
-  print(lot)
+    lot=cs_to_lot(cs)  # convert connect string to list of tuples
+    print(lot)
 
-  cs=lot_to_cs(lot)  # convert list of strings to connect string
-  print(cs)
+    cs=lot_to_cs(lot)  # convert list of strings to connect string
+    print(cs)
 
 
 if __name__ == '__main__':
