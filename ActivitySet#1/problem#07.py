a731998c65b9b4b9139f69c9b62af387ded167ae
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-06-02 09:48:25 +0000

    7,6

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,24 +1,24 @@
 # Strings
 def myinput():
-    text = "X-DSPAM-Confidence:    0.8475"
-    return text
+      text = "X-DSPAM-Confidence:    0.8475"
+      return text
 
 
 def convert(text):
-    Pos = text.find(':')
-    last = text[Pos+1:]
-    end = float(last)
-	return end
+      Pos = text.find(':')
+      last = text[Pos+1:]
+      end = float(last)
+      return end
 
 
-def output(end):	
-    print(end)
-
+def output(end):
+      print(end)
+    
 
 def main():
-    text=myinput()
-    end=convert(text)
-		output(end)
+      text=myinput()
+      end=convert(text)
+      output(end)
 
 
 main()
