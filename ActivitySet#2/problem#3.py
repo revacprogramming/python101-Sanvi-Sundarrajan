8083185e0138fdb6df2931fe0f587c5dc224bc1c
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-06-04 13:38:56 +0000

    set 2(4)

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -6,17 +6,18 @@ def get_cs():
 
 
 def cs_to_lot(cs):
-  lst=[]
+  lst1=[]
   splt1=cs.split(';')
   for i in splt1:
     splt2=i.split('=')
-    lt.append((splt2[0],splt2[1]))
-
+    lst1.append((splt2[0],splt2[1]))#appending tuple to list 
+	return lst
 
 def main():
-  cs = get_cs()
-  lot = cs_to_lot(cs)
-  print(lot)
+  lst2=[]
+    for a in lot:
+      lst2.append("=".join(a))
+    return(";".join(lst2))
 
 
 if __name__ == '__main__':
--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,15 +1,23 @@
 
 
 def get_cs():
-    cs=input("Enter a string")
-    return cs
+  cs=input("Enter a string")
+  return cs
 
 def cs_to_lot(cs):
-    lot=cs.split()
-    return lot
+  lst1=[]
+  splt1=cs.split(';')
+  for i in splt1:
+    splt2=i.split('=')
+    lst1.append((splt2[0],splt2[1]))#appending tuple to list 
+	return lst
+
 
 def lot_to_cs(lot):
-    return ''.join(lot)
+  lst2=[]
+  for a in lot:
+  	lst2.append("=".join(a))
+	return(";".join(lst2))
 
 
 def main():
