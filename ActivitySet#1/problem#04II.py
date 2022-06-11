aa08de6b1207705fc875108c59bd2509e312853a
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-06-11 15:08:18 +0000

    4II

--- ActivitySet#1/problem#04II.py
+++ ActivitySet#1/problem#04II.py
@@ -1,26 +1,34 @@
 # Conditional Execution(II)
-def input():
-	score = float(input("Enter Score: "))
-	return score
+def myinput():
+    score = float(input("Enter Score: "))
+    return score
 
 	
 def scores(s):
-	if s >= 0.9:
-		return 'A'
-	elif s >=0.8:
-		return 'B'
-	elif s >=0.7:
-		return 'C'
-	elif s >= 0.6:
-		return 'D'
-	elif s < 0.6:
-		return 'F'
-	else:
-		return "Out of Range"
+    if s>1:
+        return 'Out of range'
+    elif s<0:
+        return 'score cannot be negative'
+    elif s>=0.9:
+        return 'A'
+    elif s>=0.8:
+        return 'B'
+    elif s>=0.7:
+        return 'C'
+    elif s>=0.6:
+        return 'D'
+    elif s<0.6:
+        return 'F'
 
-		
-def main():
-	scores()
+			
+def output(res): 
+	print(res)	
 
 	
+def main():
+    s=myinput()
+    res=scores(s)
+    output(res)
+
+
 main()
